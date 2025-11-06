41b13638273ea10e61ec949c9e2c26481593b79c
William Coelho de Morais <willcmorais@gmail.com>
2025-11-06 17:39:22 -0300

    commit_02
    refatoração de código. foi criada a função exibir_subtitulo para exibir os títulos inicias quando escolhido uma opção no programa.

--- primeira_aplicacao/app.py
+++ primeira_aplicacao/app.py
@@ -4,7 +4,7 @@ import os
 # Aqui vamos criar as nossas variáveis, neste caso será um dicionário para armazenar as informações dos restaurantes com seus dados
 restaurantes = [['Pizzaloka', 'Pizza', 'William'], ['Sushibar Lounge', 'Sushi', 'Victoria']]
 
-# Funções são utilizadas para deixar o código mais limpo, e quando necessário alguma alteração, podemos fazer isso apenas na função e não nas condiconais
+# Funções são utilizadas para deixar o código mais limpo, e quando necessário alguma alteração, podemos fazer isso apenas na função e não nas condicionais
 def exibir_nome_programa():
     print("---------------------------------------------------------")
     print("=============Ｓａｂｏｒｅｓ Ｄｉｖｅｒｓｏｓ=============")
@@ -17,40 +17,38 @@ def exibir_opcoes():
     print("4. Sair.\n")
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Hora de cadastrar o seu restaurante!\n')
+    exibir_subtitulo('Área de cadastro para novos restaurantes:')
     
     nome_restaurante = input("Informe aqui o nome do seu restaurante: ")
     categoria_restaurante = input("Informe aqui o tipo de comida que seu restaurante oferece: ")    
     nome_proprietario_restaurante = input("Informe aqui o nome do proprietário do restaurante: ")
 
     restaurantes.append([nome_restaurante, categoria_restaurante, nome_proprietario_restaurante])
-    
+
     print(f'\nRestaurante {nome_restaurante} cadastrado com sucesso!')
     
-    input('\nPressione Enter para voltar ao menu principal.')
-    main()
+    voltar_ao_menu()
 
 def listar_restaurantes():
-    os.system("cls")
-    print('Lista de restaurantes cadastrados:\n')
+    exibir_subtitulo('Listagem de restaurantes cadastrados:')
+    
     for restaurante in restaurantes:
         print(f'. {restaurante}')
 
-    input("\nPressione Enter para voltar ao menu principal.")
-    main()
+    voltar_ao_menu()
 
 def ativar_novo_restaurante():
-    print('Hora de ativar o seu restaurante!')
-
-def encerrar_programa():
-    os.system("cls")
-    print("Encerrando programa...\n")
+    exibir_subtitulo('Hora de ativar o seu restaurante!')
+    
+    voltar_ao_menu()
 
 def opcao_invalida():
-    print('Opção inválida!\n')
-    input('Pressione Enter para voltar ao menu principal.')
-    main()
+    exibir_subtitulo("Opção Inválida!")
+    
+    voltar_ao_menu()
+
+def encerrar_programa():
+    exibir_subtitulo('Encerrando programa...')
 
 def escolher_opcao():
     try:
@@ -69,7 +67,7 @@ def escolher_opcao():
                 opcao_invalida()
     except:
         opcao_invalida()
-                        
+
     # if opcao_escolhida == 1:
     #     cadastrar_restaurante()
     # elif opcao_escolhida == 2:
@@ -78,6 +76,15 @@ def escolher_opcao():
     #     ativar_restaurante()
     # else:
     #     encerrar_programa()
+
+def voltar_ao_menu():
+    input('Pressione Enter para voltar ao menu principal ')
+    main()
+
+def exibir_subtitulo(texto):
+    os.system("cls")
+    print(texto)
+    print()
 
 def main():
     os.system('cls')
