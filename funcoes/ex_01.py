711d799862ab9e0b78bcfe459284e38a60fbc0c7
Willcmorais <willcmorais@gmail.com>
2025-11-18 14:36:13 -0300

    commit_01.1
    Início dos exercícios de funções.  ex_01.py

---
+++ funcoes/ex_01.py
@@ -0,0 +1,4 @@
+# Julia é professora e precisa de um programa para ajudar seus alunos a calcularem suas idades com base no ano de nascimento. Sua tarefa é criar uma função que receba o ano de nascimento e o ano atual e retorne à idade correspondente.
+
+def idade_atual(ano_nascimento, ano_atual):
+    
