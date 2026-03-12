f3dc97fd96677382d2d6ebfa3f6419ec1d38d5d1
Willcmorais <willcmorais@gmail.com>
2026-03-11 21:31:17 -0300

    Return CPF validation message; print in main

--- praticando_projetos/projeto_04/main_projeto_04.py
+++ praticando_projetos/projeto_04/main_projeto_04.py
@@ -2,4 +2,4 @@ from projeto_04_validar_cpf import validar_cpf
 
 cpf = input("Informe aqui seu CPF: ")
 
-validar_cpf(cpf)
+print(validar_cpf(cpf))
--- praticando_projetos/projeto_04/projeto_04_validar_cpf.py
+++ praticando_projetos/projeto_04/projeto_04_validar_cpf.py
@@ -1,6 +1,6 @@
 def validar_cpf(cpf):
     if not cpf.isdigit():
-        return print("Erro: O CPF deve conter apenas números.")
+        return "Erro: O CPF deve conter apenas números."
     if len(cpf) != 11:
-        return print("Erro: O CPF deve ter exatamente 11 dígitos.")
-    return print("CPF válido.")
+        return "Erro: O CPF deve ter exatamente 11 dígitos."
+    return "CPF válido."
