e80d712a0e022179e993e2407810b36efe7226df
William Coelho de Morais <willcmorais@gmail.com>
2026-03-12 17:51:51 -0300

    Add jokenpo main and escolher_opcao
    Note: Its not complete yet, it's the initial development.

---
+++ praticando_projetos/projeto_08_jokenpo/main_projeto_08.py
@@ -0,0 +1,5 @@
+from projeto_08_jokenpo import escolher_opcao
+
+print("Escolha uma das opções abaixo:\n\n1 - Pedra\n2 - Papel\n3 - Tesoura\n")
+
+escolher_opcao()
---
+++ praticando_projetos/projeto_08_jokenpo/projeto_08_jokenpo.py
@@ -0,0 +1,4 @@
+def escolher_opcao():
+    opcao_escolhida = int(input("Informe sua jogada: "))
+
+    return opcao_escolhida
