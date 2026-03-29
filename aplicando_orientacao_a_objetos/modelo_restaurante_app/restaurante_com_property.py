9b9e0847002ea0a89d5f846a3d97ce6ed35dfa50
Willcmorais <willcmorais@gmail.com>
2026-03-29 16:04:55 -0300

    Add docstrings in restaurante file

--- aplicando_orientacao_a_objetos/modelo_restaurante_app/restaurante_com_property.py
+++ aplicando_orientacao_a_objetos/modelo_restaurante_app/restaurante_com_property.py
@@ -2,9 +2,20 @@ from avaliacao_restaurante import Avaliacao
 
 
 class Restaurante:
+    """Representa um restaurante e suas características."""
+
     lista_de_restaurantes = []
 
     def __init__(self, nome="", categoria="", horario_funcionamento=""):
+        """
+        Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (str): O nome do restaurante.
+        - categoria (str): A categoria do restaurante.
+        - horario_funcionamento (str): O horário de funcionamento do restaurante.
+        """
+
         # atributos protegidos
         self._nome = nome.title()
         self._categoria = categoria.title()
@@ -14,10 +25,12 @@ class Restaurante:
         Restaurante.lista_de_restaurantes.append(self)
 
     def __str__(self):
+        """Retorna uma representação em string do restaurante."""
         return f"{self._nome} | {self._categoria} | {self._horario_funcionamento} | {self._status}"
 
     @classmethod
     def listar_restaurantes(cls):
+        """Exibe uma lista formatada de todos os restaurantes."""
         print("-" * 48, "LISTA DE RESTAURANTES", "-" * 48, "\n")
         # Como estamos fazendo operações com os nomes, colocamos eles entre { }. Caso contrário não conseguiríamos justificar igual ao print do loop
         print(
@@ -32,10 +45,12 @@ class Restaurante:
     # O property é usado para encapsular o atributo da classe, e criar a partir dele um método. Esse método vai fazer uma certa ação de acordo com a lógica criada, modificando a visualização padrão
     @property
     def status(self):
+        """Retorna um símbolo indicando o estado de atividade do restaurante."""
         return "✅" if self._status else "❎"
 
     @property
     def media_avaliacoes(self):
+        """Calcula e retorna a média das avaliações do restaurante."""
         # se não tivermos nenhuma avaliação vai retornar zero
         if not self._avaliacao:
             return "-"
@@ -46,10 +61,18 @@ class Restaurante:
         return media_das_notas
 
     def receber_avaliacao(self, cliente, nota):
+        """
+        Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (str): O nome do cliente que fez a avaliação.
+        - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
+        """
         if 0 <= nota <= 5:
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
 
     # alterna o status, se for True fica False e vice-versa
     def alternar_status(self):
+        """Alterna o estado de atividade do restaurante."""
         self._status = not self._status
