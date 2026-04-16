89651e1ec3dc624c04558dc4b36699a84fcb0fd9
Willcmorais <willcmorais@gmail.com>
2026-04-16 13:26:20 -0300

    Rewrite the output message for the values.
    Create variables with the currency data, converting the numbers from string to float. This allows us to format the message so that it displays only two decimal places after the decimal point.

--- venv_testes_de_api/app2.py
+++ venv_testes_de_api/app2.py
@@ -1,13 +1,23 @@
 import requests
 import json
 
-url = "https://economia.awesomeapi.com.br/last/BRL-ARS"
+url = "https://economia.awesomeapi.com.br/last/USD-BRL"
 response = requests.get(url)
 
 if response.status_code == 200:
     dados_json = response.json()
 
 for conversao, dados in dados_json.items():
+    codigo_moeda = dados["code"]
+    codigo_moeda_ref = dados["codein"]
+    nome_moeda = dados["name"]
+    val_mais_alto = float(dados["high"])
+    val_mais_baixo = float(dados["low"])
+    variacao_lance = float(dados["varBid"])
+    percentual_mudanca = float(dados["pctChange"])
+    preco_compra = float(dados["bid"])
+    preco_venda = float(dados["ask"])
+
     print(
-        f"==== {conversao} ====\nCódigo da moeda: {dados["code"]}\nCódigo da moeda de referência: {dados["codein"]}\nNome da moeda: {dados["name"]}\nValor mais alto: {dados["high"]}\nValor mais baixo: {dados["low"]}\nVariação do lance: {dados["varBid"]}\nPecentual de mudança: {dados['pctChange']}\nPreço de compra: {dados["bid"]}\nPreço de venda: {dados["ask"]}\nCarimbo da data/hora: {dados["timestamp"]}\nData e hora do registro: {dados["create_date"]}"
+        f"==== {conversao} ====\nCódigo da moeda: {codigo_moeda}\nCódigo da moeda de referência: {codigo_moeda_ref}\nNome da moeda: {nome_moeda}\nValor mais alto: {val_mais_alto:.2f}\nValor mais baixo: {val_mais_baixo:.2f}\nVariação do lance: {variacao_lance:.2f}\nPecentual de mudança: {percentual_mudanca:.2f}\nPreço de compra: {preco_compra:.2f}\nPreço de venda: {preco_venda:.2f}\nCarimbo da data/hora: {dados["timestamp"]}\nData e hora do registro: {dados["create_date"]}"
     )
